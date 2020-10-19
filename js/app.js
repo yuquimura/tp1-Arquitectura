30d19d7065fcaa697c63822fa48e61e19b48e0ca
Yuquimura <brianzambellitello@gmail.com>
2020-10-19 16:41:42 -0300

    Agregado de app.js version inicial

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const PORT = 3000;
+const TIMEOUT = 5000;
+const ID = Math.floor(Math.random()*1000);
+const app = express();
+app.get('/',(req,res)=> {
+    res.status(200).send('ping - '+ ID);
+});
+
+app.get('/timeout', (req,res) => {
+    setTimeout(() => {
+        res.status(200).send('timeout - ' + ID);
+    },TIMEOUT);
+});
+
+app.listen(PORT, () => {
+    CONSOLE.LOG('Escuchando en el puerto', PORT)
+});
