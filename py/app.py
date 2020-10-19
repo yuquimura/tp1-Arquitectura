6b48959db72164ebf740e580a3717cb6a4880dcd
Yuquimura <brianzambellitello@gmail.com>
2020-10-19 17:10:21 -0300

    Escritura de app.py modalidad Timeout

--- py/app.py
+++ py/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import time
 TIMEOUT = 5
 
 app = Flask( __name__)
@@ -7,6 +7,11 @@ app = Flask( __name__)
 @app.route('/')
 def raiz():
     return "Ping"
+
+@app.route('/timeout')
+def esperar():
+    time.sleep(TIMEOUT)
+    return "Timeout"
 
 if __name__ == "__main__":
     app.run()
